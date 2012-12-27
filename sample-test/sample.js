5e2336d84a0677497f19a1a27c320e31816f470b
Ryan O'Neill <rco8786@gmail.com>
2012-12-27 13:50:17 -0500

    working through phantom implemenation

--- sample-test/sample.js
+++ sample-test/sample.js
@@ -4,11 +4,15 @@ module.exports.setUp = function() {
 
 module.exports.tests = [
 	function sampleTest1(assert) {
+		
 		assert.areEqual(2, 2);
 		assert.areEqual(3, 3);
+		return assert;
 	},
 	function sampleTest2(assert) {
+		
 		assert.areEqual("Hello", "Goodbye");
+		return assert;
 	}
 ];
 
--- sample-test/sample2.js
+++ sample-test/sample2.js
@@ -4,10 +4,14 @@ module.exports.setUp = function() {
 
 module.exports.tests = [
 	function sampleTest11(assert) {
-		assert.areEqual(1, 2);
+		
+			assert.areEqual(1, 2);
+		return assert;
 	},
 	function sampleTest21(assert) {		
+		
 		assert.areEqual("Hello", "Hello");
+		return assert;
 	}
 ];
 
